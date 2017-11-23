8e78005288ac3176f50d21453548cbaf6a5371eb
MrSaqibAmeen <mrsaqibameen@gmail.com>
2017-11-23 13:54:40 +0500

    ADD: rest-and-spread.js

---
+++ output/rest-and-spread.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Rest allows to take undetermined number of inputs.
+// Spread splits single array into multiple items.
+
+// Rest implementation.
+
+// Array sum function.
+function sum() {
+	for (var _len = arguments.length, numbers = Array(_len), _key = 0; _key < _len; _key++) {
+		numbers[_key] = arguments[_key];
+	}
+
+	return numbers.reduce(function (prev, current) {
+		return prev + current;
+	}); // Arrow function used.
+}
+
+// Sum function call.
+console.log(sum(1, 2, 4, 6)); // Add as much argument as you want.
+
+
+// Spread implementation.
+
+// 2 numbers sum function.
+function sumSeparate(x, y) {
+	return x + y;
+}
+
+// Array of numbers to be summed.
+var num = [3, 5];
+
+// Call separate sum function.
+console.log(sumSeparate.apply(undefined, num));
---
+++ src/rest-and-spread.js
@@ -0,0 +1,26 @@
+// Rest allows to take undetermined number of inputs.
+// Spread splits single array into multiple items.
+
+// Rest implementation.
+
+// Array sum function.
+function sum(...numbers) {
+	return numbers.reduce((prev, current) => prev + current); // Arrow function used.
+}
+
+// Sum function call.
+console.log(sum(1, 2, 4, 6)); // Add as much argument as you want.
+
+
+// Spread implementation.
+
+// 2 numbers sum function.
+function sumSeparate(x,y) {
+	return x + y;
+}
+
+// Array of numbers to be summed.
+let num = [3, 5];
+
+// Call separate sum function.
+console.log(sumSeparate(...num));
